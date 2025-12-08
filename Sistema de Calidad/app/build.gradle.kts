4e3e33bc5429774d60d64275dd6b049b1c182bca
milith0kun <milith0kun@gmail.com>
2025-12-08 02:23:33 -0500

    Bump app version to 1.0.8 (versionCode 9)
    Updated build.gradle.kts to set versionCode to 9 and versionName to 1.0.8. Also updated Gradle build and report files as part of the build process.

--- Sistema de Calidad/app/build.gradle.kts
+++ Sistema de Calidad/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.sistemahaccp.calidad"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
